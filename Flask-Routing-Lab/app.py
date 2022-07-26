3e03365db37ed20637461bc6e83d348d0b11932f
yuvalv23meet <92042419+yuvalval24@users.noreply.github.com>
2022-07-26 10:47:12 +0300

    starting out completed flask part

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -8,8 +8,17 @@ app = Flask(  # Create a flask app
 )
 
 # Your code should be below
+@app.route("/")
+def home():
+    return render_template("home.html")
 
+@app.route("/product")
+def product():
+    return render_template("product.html")
 
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
 
 
 # Your code should be above
